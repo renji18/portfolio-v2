499d58a5e15f49f4a48a497a0f790a9ebfce1198
renji18 <aadarshjha1401@gmail.com>
2025-02-17 23:32:01 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,16 @@ const App = () => {
   // use effect to get portfolio data
   useEffect(() => {
     async function fetchData() {
+      window.scrollTo(0, 0)
+      document.documentElement.style.scrollBehavior = "smooth"
+      const scrollableContainer = document.querySelector("#home")
+      if (scrollableContainer) scrollableContainer.scrollTop = 0
+
       const response = await handleGetPortfolioData()
       setPortfolioData(response)
     }
     fetchData()
   }, [])
-
-  console.log(portfolioData)
 
   return (
     <>
