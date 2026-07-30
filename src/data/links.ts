c81d8d679d26b5134fb36d85d4267e1d6a30a815
renji18 <aadarshjha1401@gmail.com>
2026-07-30 23:47:06 +0530

    updated resume

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,5 +1,5 @@
 export const LINKEDIN = "https://www.linkedin.com/in/aadarshJha1401/";
 
-export const RESUME = "https://drive.google.com/file/d/1QoeZfMMK6S7BKAL_i10foKT7gXgzuoGT/view?usp=sharing";
+export const RESUME = "https://drive.google.com/file/d/1imaV2GqckqozvOs3dKslCfBo4foBl9u2/view?usp=sharing";
 
 export const GITHUB = "https://github.com/renji18";
